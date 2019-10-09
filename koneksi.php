ab8791f00374b40753e7b45e438328300fef5268
zenn04 <55547943+zenn04@users.noreply.github.com>
2019-10-09 23:40:08 +0700

    entah butuh opo gak

---
+++ koneksi.php
@@ -0,0 +1,7 @@
+<?php
+$host = "localhost"; // Nama hostnya
+$user = "root"; // Username
+$pass = ""; // Password (Isi jika menggunakan password)
+$db_name = "chat";
+$connect = mysqli_connect($host, $user, $pass, $db_name); // Koneksi ke MySQL
+?>
